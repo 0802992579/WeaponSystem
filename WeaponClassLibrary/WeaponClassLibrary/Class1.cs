73b9067f526e69d6892f42f69b364a7cf92d9a84
0802992579 <olafura@live.com>
2017-04-13 23:11:56 +0000

    First code

---
+++ WeaponClassLibrary/WeaponClassLibrary/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeaponClassLibrary
+{
+    public class Weapon
+    {
+        public string Name { get; set; }
+        public int Weight { get; set; }
+        public string DamageType { get; set; }
+        public int Damage { get; set; }
+        public int Range { get; set; }
+        public int Value { get; set; }
+        public string Modifier { get; set; }
+
+        public Weapon(string name, int weight, string damagetype, int damage, int range, int value, string modifier)
+        {
+            Name = name;
+            Weight = weight;
+            DamageType = damagetype;
+            Damage = damage;
+            Range = range;
+            Value = value;
+            Modifier = modifier;
+    }
+
+        public override string ToString()
+        {
+            return string.Format("Name: {0}\nWeight: {1}\nDamageType: {2}\nDamage: {3}\nRange: {4}\nValue: {5}\nModifier",
+                Name, Weight, DamageType, Damage, Range, Value, Modifier);
+        }
+    }
+}
